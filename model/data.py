b5d99a7973e2bc59a56d62820c35198dea73cccc
Akash <Akash>
2024-01-03 21:44:14 +0000

    completed trainpy, linted, removed unnecessary files

--- model/data.py
+++ model/data.py
@@ -1,39 +1,50 @@
+from typing import Self
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-
-class ExperimentBuilder(nn.Module):
-    def __init__(self, network_model, experiment_name, num_epochs, train_data, val_data,
-                 test_data, weight_decay_coefficient, use_gpu, continue_from_epoch=-1):
+import torch
 
 class Data():
     
     def __init__(self, 
-                data : str = 'cifar10'
+                data : str = 'cifar10',
                 batch_size: int = 100):
     
-    self.data = data,
+        self.data = data,
+        self.batch_size = batch_size
     
-    self.batch_size = batch_size
+    def transform_train(self):
         
-    def make_data():
+        return transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
         
-        transform_train = transforms.Compose([
-                transforms.RandomCrop(32, padding=4),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
+    def transform_val(self):
         
-        transform_val = transforms.Compose([
+        return transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
         
-        cifar_train= datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-        cifar_val = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_val)    
-        trainloader = torch.utils.data.DataLoader(cifar_train, batch_size=self.batch_size,
+    def make_data(self):
+        
+        self.train = datasets.CIFAR10(root='./data', train=True, download=True, transform=self.transform_train())
+
+        print(len(self.train))        
+
+        self.len_train = len(self.train)
+        
+        self.val = datasets.CIFAR10(root='./data', train=False, download=True, transform=self.transform_val())
+        
+        self.len_val = len(self.val)
+        
+        trainloader = torch.utils.data.DataLoader(self.train, batch_size=self.batch_size,
                                           shuffle=True, num_workers=4)
-        valloader = torch.utils.data.DataLoader(cifar_val, batch_size=self.batch_size,
-                                         shuffle=True, num_workers=4)
         
+        valloader = torch.utils.data.DataLoader(self.val, batch_size=self.batch_size,
+                                         shuffle=True, num_workers=4)
+     
         return trainloader, valloader
+    
+    def get_length(self):
+        return self.len_train, self.len_val
--- model/train.py
+++ model/train.py
@@ -1,76 +1,128 @@
 import torch
 from torchvision.models.detection import retinanet_resnet50_fpn
+from torchvision.ops import batched_nms
 from torchvision.transforms import functional as F
 from PIL import Image
 from torch.utils.data import DataLoader
 from torchvision.models import resnet50, ResNet50_Weights
 import torch.optim as optim
 from torch import nn
-import tqdm
+from tqdm import tqdm
+import numpy as np
 
 
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+import data
 
 
-if __name__ == '__main__':
 
- 
+if __name__ == "__main__":
+    # get data
+    batch_size = 100
+    d = data.Data(batch_size=batch_size)
+    trainloader, valloader = d.make_data()
+    len_train, len_val = d.get_length()
+
+    # define model
     resnet50(weights=ResNet50_Weights.DEFAULT)
-    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    print(device)
-
     model = resnet50()
-    
     model.to(device)
-    
     criterion = nn.CrossEntropyLoss()
-    
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    
-    for epoch in range(2):  # loop over the dataset multiple times
 
-        running_loss = 0.0
-    
-        with tqdm.tqdm(total=len(cifar_train)/batch_size) as pbar_train:
-            
+    epochs = 2
+
+    acc_train = []
+    loss_train = []
+    acc_val = []
+    loss_val = []
+
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        with tqdm(total=len(trainloader)) as pbar_train:
+            model.train()
+            train_acc_collect = []
+            train_loss_collect = []
+
             for i, data in enumerate(trainloader, 0):
-        
                 # get the inputs; data is a list of [inputs, labels]
-        
+
                 inputs, labels = data
-                
                 inputs = inputs.cuda()
-                
                 labels = labels.cuda()
-                
-                #print(len(data))
-        
+
                 # zero the parameter gradients
-        
+
                 optimizer.zero_grad()
 
                 # forward + backward + optimize
-        
+
                 outputs = model(inputs)
-                
                 loss = criterion(outputs, labels)
-                
                 loss.backward()
-                
                 optimizer.step()
-        
+
                 # print statistics
-                
-                running_loss += loss.item()
-                    
+
+                _, predicted = torch.max(outputs.data, 1)  # get argmax of predictions
+                accuracy = np.mean(
+                    list(predicted.eq(labels.data).cpu())
+                )  # compute accuracy
+                train_acc_collect.append(accuracy)
+                train_loss_collect.append(loss.cpu().data.numpy())
+
+                # print('LOSS_TRAIN: ',loss.cpu().data.numpy(),'  ','ACC_TRAIN: ',accuracy, flush = True)
+
                 pbar_train.update(1)
-                
-                pbar_train.set_description("loss: {:.4f}")
+                pbar_train.set_description("Iterations")
 
+            avg_acc_train = np.mean(train_acc_collect)
+            avg_loss_train = np.mean(train_loss_collect)
+            print(
+                "LOSS_TRAIN_epoch {}: ".format(epoch),
+                avg_acc_train,
+                "  ",
+                "ACC_TRAIN_epoch {}: ".format(epoch),
+                avg_loss_train,
+            )
+            acc_train.append(avg_acc_train)
+            loss_train.append(avg_loss_train)
 
+        with tqdm(total=len(valloader)) as pbar_val:
+            model.eval()
+            vall_acc_collect = []
+            vall_loss_collect = []
 
+            for i, data in enumerate(valloader, 0):
+                # get the inputs; data is a list of [inputs, labels]
 
+                inputs, labels = data
+                inputs = inputs.cuda()
+                labels = labels.cuda()
+
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                _, predicted = torch.max(outputs.data, 1)  # get argmax of predictions
+                accuracy = np.mean(
+                    list(predicted.eq(labels.data).cpu())
+                )  # compute accuracy
+                vall_acc_collect.append(accuracy)
+                vall_loss_collect.append(loss.cpu().data.numpy())
+
+                pbar_val.update(1)
+                pbar_val.set_description("Iterations")
+
+            avg_acc_val = np.mean(vall_acc_collect)
+            avg_loss_val = np.mean(vall_loss_collect)
+            print(
+                "LOSS_VAL_epoch {}: ".format(epoch),
+                avg_loss_val,
+                "  ",
+                "ACC_VAL_epoch {}: ".format(epoch),
+                avg_acc_val,
+            )
+            acc_val.append(avg_acc_val)
+            loss_val.append(np.mean(avg_loss_val))
