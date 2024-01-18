8de729b16b1f21d226b456802cb135aaed39f637
Akash <Akash>
2024-01-18 17:38:37 +0000

    error add

--- model/train.py
+++ model/train.py
@@ -122,6 +122,6 @@ if __name__ == "__main__":
                 avg_loss_val,
                 "  ",
                 "ACC_VAL_epoch {}: ".format(epoch),
-                avg_acc_val,)
+                avg_acc_val, )
             acc_val.append(avg_acc_val)
             loss_val.append(np.mean(avg_loss_val))
