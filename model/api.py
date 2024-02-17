f658afec2df7b1042033f9391b2b54dbaef9c563
Akash <Akash>
2024-02-17 15:04:21 +0000

    add

--- model/api.py
+++ model/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 @app.get("/webhook")
+
 async def root():
     return {
         "This is a local endpoint to recieve webhook payloads from github. Ngrok is used for tunneling."
